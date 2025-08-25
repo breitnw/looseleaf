0b134a148646212d343050f6498fdb1006fb557c
Nick Breitling <breitling.nw@gmail.com>
2025-08-24 23:01:45 -0500

    public api section, better organization, some setup function headers

--- src/looseleaf.h
+++ src/looseleaf.h
@@ -4,19 +4,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// HEADER ======================================================================
+//    +----------+
+//   /  HEADER  /
+// -+----------+----------------------------------------------------------------
+
+// TODO find a better place for this
+#ifndef LL_IMAGE_TYPE
+#define LL_IMAGE_TYPE void
+#endif
 
 // initialization stage ========================================================
 // --> create the memory arena and context
 
 typedef struct ll_Context ll_Context;
-
-ll_Context ll_init(char* arena_mem, size_t arena_capacity);
-
-// setup stage =================================================================
-// --> initialize ephemeral memory and prepare the context
-
-void ll_begin_record(ll_Context* ctx);
 
 // recording stage =============================================================
 // --> instantiate nodes and add them to the context
@@ -46,20 +46,13 @@ typedef struct {
 
 // types of nodes --------------------------------------------------------------
 
-// configuration for each node type --------------------------------------------
-
 typedef uint32_t ll_NodeHandle;
 
+// HACK no support for newlines as of now
 typedef struct {
-  const char* text;
   // Additional spacing between letters (can be negative)
-  int16_t letter_spacing; // don't really need support for newlines
+  int16_t letter_spacing;
 } ll_TextConfig;
-
-// TODO move this
-// Provided a single line of text and a pixel spacing between letters, return
-// the dimensions of that line in pixels.
-ll_Size ll_measure_text(const char* text, uint16_t letter_spacing);
 
 typedef struct {
   ll_HorizAlign align_h;
@@ -81,14 +74,14 @@ typedef struct {
   ll_Vec2 offset;
 } ll_MovePinholeConfig;
 
+// the node struct -------------------------------------------------------------
+
 typedef struct ll__Node {
-  // A union describing the required data of a node.
-  // A node's data is the required information about its children, or the
-  // specific image or text that will be rendered.
-  union Data {
+  // A union describing the contents (image, text, or children) of a node.
+  union Contents {
     // The data of this node, if it is an image node
     struct ImageData {
-      void *image_data;
+      LL_IMAGE_TYPE* image_data;
       ll_Size image_size;
     };
     // The text contents of this node, if it is a text node
@@ -104,8 +97,7 @@ typedef struct ll__Node {
     } children;
   } data;
 
-  // A union describing the configuration of a node. A node's configuration
-  // is the optional styling information specific to its type.
+  // A union describing the optional configuration of a node.
   union Config {
     ll_TextConfig text_config;
     ll_AboveConfig above_config;
@@ -114,7 +106,7 @@ typedef struct ll__Node {
     ll_MovePinholeConfig move_pinhole_config;
   } config;
 
-  // TODO consolidate the two unions above into one
+  // TODO consolidate the two unions above into one?
 
   // An enum describing a node's type.
   enum Tag {
@@ -137,16 +129,9 @@ typedef struct ll__NodeArray {
   ll__Node* internalArray;
 } ll__NodeArray;
 
-// child nodes always come last as parameters, in case function calls are nested
-ll_NodeHandle ll_image(void* image_data);
-ll_NodeHandle ll_text(const char* text, ll_TextConfig conf);
-ll_NodeHandle ll_above(ll_AboveConfig conf, ll_NodeHandle above, ll_NodeHandle below);
-ll_NodeHandle ll_beside(ll_BesideConfig conf, ll_NodeHandle left, ll_NodeHandle right);
-ll_NodeHandle ll_overlay(ll_OverlayConfig conf, ll_NodeHandle over, ll_NodeHandle under);
-ll_NodeHandle ll_move_pinhole(ll_MovePinholeConfig conf);
-ll_NodeHandle ll_reset_pinhole();
 
-// draw finalization stage: convert node tree to iterable array ================
+// draw finalization stage =====================================================
+// --> convert node tree to iterable array
 
 // Tag representing a type of render command
 typedef enum {
@@ -169,13 +154,13 @@ typedef union {
 } ll_RenderDataUnion;
 
 typedef struct {
-  int32_t x, y;
-  uint32_t w, h;
+  ll_Vec2 posn;
+  ll_Size size;
 } ll_Bounds;
 
 typedef struct {
-  ll_RenderDataTag tag;
   ll_Bounds bounds;
+  ll_RenderDataTag tag;
   ll_RenderDataUnion render_data;
 } ll_RenderCommand;
 
@@ -189,11 +174,10 @@ typedef struct {
   ll_RenderCommand* internalArray;
 } ll_RenderCommandArray;
 
-ll_RenderCommandArray ll_end_record();
 
-// data ------------------------------------------------------------------------
+// context data ================================================================
 
-typedef struct ll__Arena {
+typedef struct {
   uintptr_t next_alloc;
   size_t capacity;
   char* mem;
@@ -205,9 +189,74 @@ struct ll_Context {
   ll__NodeArray nodes;
 };
 
-// IMPLEMENTATION ==============================================================
 
-ll_Context ll_init(char* arena_mem, size_t arena_capacity) {
+// public function API =========================================================
+
+// setup and initialization...
+
+// Configure the function looseleaf uses to measure text.
+// Required before creating a context.
+void ll_set_text_measurement_fn(ll_Size (*text_measurement_fn)(const char* text, uint16_t letter_spacing));
+// Configure the function looseleaf uses to measure images.
+// Required before creating a context.
+void ll_set_image_measurement_fn(ll_Size (*image_measurement_fn)(LL_IMAGE_TYPE* image));
+// Configure the maximum number of nodes that can be "in flight" at a given time
+void ll_configure_max_nodes(uint32_t max_nodes);
+// Return the minimum size of an arena used to initialize the looseleaf context
+uint64_t ll_min_arena_size(void);
+// Initialize a looseleaf context from a memory arena
+// TODO error if measurement functions aren't set up properly
+ll_Context* ll_init(char* arena_mem, size_t arena_capacity);
+
+// per-frame recording...
+
+// Clear the looseleaf context and set it up for recording
+void ll_begin(ll_Context* ctx);
+
+// DESIGN: data comes after configuration, in case function calls are nested
+
+// Allocate a leaf representing an image, with data defined by LL_IMAGE_TYPE
+ll_NodeHandle ll_image(LL_IMAGE_TYPE* image_data, ll_Size image_size);
+// Allocate a leaf represeting a string of text
+ll_NodeHandle ll_text(ll_TextConfig conf, const char* text);
+// Allocate a binary node that renders the first node above the second
+ll_NodeHandle ll_above(ll_AboveConfig conf, ll_NodeHandle above, ll_NodeHandle below);
+// Allocate a binary node that renders the first node to the left of the second
+ll_NodeHandle ll_beside(ll_BesideConfig conf, ll_NodeHandle left, ll_NodeHandle right);
+// Allocate a binary node that renders the first node on top of the second
+ll_NodeHandle ll_overlay(ll_OverlayConfig conf, ll_NodeHandle over, ll_NodeHandle under);
+// Allocate a unary node that moves a node's pinhole as defined by `conf`
+ll_NodeHandle ll_move_pinhole(ll_MovePinholeConfig conf, ll_NodeHandle node);
+// Allocate a unary node that resets a node's pinhole to its original position
+ll_NodeHandle ll_reset_pinhole(ll_NodeHandle node);
+
+// Generate an iterable array of render commands from an ll_NodeHandle
+ll_RenderCommandArray ll_gen_commands(ll_NodeHandle root);
+
+
+//    +------------------+
+//   /  IMPLEMENTATION  /
+// -+------------------+--------------------------------------------------------
+
+// program state (ugly, gross, disgraceful) ====================================
+
+ll_Context* ll__current_context;
+uint32_t ll__max_nodes = 4096;
+
+// private functions ===========================================================
+
+// TODO find a home for these
+
+// Provided a single line of text and a pixel spacing between letters, return
+// the dimensions of that line in pixels.
+ll_Size ll__measure_text(const char* text, uint16_t letter_spacing);
+
+// Provided an instance of LL_IMAGE_TYPE, return the pixel size of that image
+ll_Size ll__measure_image(LL_IMAGE_TYPE* image);
+
+// public functions ============================================================
+
+ll_Context* ll_init(char* arena_mem, size_t arena_capacity) {
   ll__Arena arena = (ll__Arena){
       .capacity = arena_capacity,
       .mem = arena_mem,
@@ -216,20 +265,22 @@ ll_Context ll_init(char* arena_mem, size_t arena_capacity) {
   return (ll_Context) { .arena = arena, };
 }
 
+
+// EXAMPLE =====================================================================
+
+#define SIZE 4096
+
 int main() {
-  #define SIZE 10000
   char arena[SIZE];
   ll_Context ctx = ll_init(arena, SIZE);
-  ll_begin_record(&ctx); // TODO rename to "free"?
+  ll_begin(&ctx);
 
-  // TODO it seems that some of these parameters aren't required but should be...
-  // may need to split up "config" and "data" for node types
-  ll_NodeHandle im = ll_image({.image_data = NULL, .width = 1, .height = 1});
+  ll_NodeHandle im = ll_image(NULL, {.width = 1, .height = 1});
   ll_NodeHandle over = ll_overlay(
       {.align_h = LL_HORIZ_ALIGN_LEFT},
-      ll_text({.text = "hello world!"}),
-      ll_beside({.align_h = LL_HORIZ_ALIGN_CENTER}, im, im)
+      ll_text({.letter_spacing = 3}, "hello world"),
+      ll_beside({.align_v = LL_VERT_ALIGN_CENTER}, im, im)
   );
 
-  ll_end_record(); // TODO rename to "to_commands"?
+  ll_RenderCommandArray cmds = ll_gen_commands(over);
 }
